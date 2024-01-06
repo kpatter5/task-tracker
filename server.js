927185501ebdfcfef86a714864efa6cd7f267558
kpatter5 <kpatter5@binghamton.edu>
2024-01-05 20:50:06 -0500

    changed port to env variable 80

--- server.js
+++ server.js
@@ -146,6 +146,7 @@ app.use((err, req, res, next) => {
     res.render('error', { err });
 });
 
-app.listen(3000, () => {
-    console.log("LISTENING ON PORT 3000...");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`LISTENING ON PORT ${port}...`);
 })
